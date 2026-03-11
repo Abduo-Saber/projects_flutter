6797a1ad48c6f9b52a3c1271495fa70d0c8020be
Abduo-Saber <140552388+Abduo-Saber@users.noreply.github.com>
2026-03-11 07:23:15 -0200

    Update task42.dart

--- task42.dart
+++ task42.dart
@@ -2,14 +2,14 @@ import 'dart:math';
 import 'dart:io';
 
 void main() {
-    // User specifies the range
+   
     print('Enter the smallest number:');
     int min = int.parse(stdin.readLineSync() !);
 
     print('Enter the largest number:');
     int max = int.parse(stdin.readLineSync() !);
 
-    // Program generates two random numbers
+    
     Random random = Random();
     int num1 = min + random.nextInt(max - min + 1);
     int num2 = min + random.nextInt(max - min + 1);
@@ -17,12 +17,13 @@ void main() {
     print('What is $num1 * $num2?');
     int userAnswer = int.parse(stdin.readLineSync() !);
 
-    // Check the answer
+   
     int correctAnswer = num1 * num2;
 
     if (userAnswer == correctAnswer) {
-        print('✅ Correct answer!');
+        print(' Correct answer!');
     } else {
-        print('❌ Wrong answer. The correct answer is: $correctAnswer');
+        print(' Wrong answer. The correct answer is: $correctAnswer');
     }
+
 }
